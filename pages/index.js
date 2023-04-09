b7989880ff94af4ec8ac098084f4eb57b487e3de
James <jamesrlew@gmail.com>
2023-04-09 15:39:34 -0500

    Remove Awards

--- pages/index.js
+++ pages/index.js
@@ -39,18 +39,18 @@ export async function getStaticProps(context) {
   //     headers,
   //   }
   // ).then((res) => res.json());
-  const rAwards = await fetch(
-    `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Awards?view=Grid%20view`,
-    {
-      headers,
-    }
-  ).then((res) => res.json());
+  // const rAwards = await fetch(
+  //   `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Awards?view=Grid%20view`,
+  //   {
+  //     headers,
+  //   }
+  // ).then((res) => res.json());
 
   const resume = {
     // skills: rSkills.records,
-    awards: rAwards?.records,
-    education: rEducation?.records,
-    employers: rEmployers?.records,
+    // awards: rAwards.records,
+    education: rEducation.records,
+    employers: rEmployers.records,
   };
 
   const posts = await api.posts.browse({
