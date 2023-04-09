4c42a08dd2979a58cb17b8555a06277ea30e2503
James <jamesrlew@gmail.com>
2023-04-09 16:32:41 -0500

    Implement Google Tag

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "../styles/globals.css";
 import Layout from "../components/layout";
+import Script from "next/script";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,19 @@ function Portfolio({ Component, pageProps }) {
           content='minimum-scale=1, initial-scale=1, width=device-width'
         />
       </Head>
+      <Script
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=G-5PHM3QTS21'
+      />
+      <Script id='google-tag-manager' strategy='afterInteractive'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-5PHM3QTS21');
+        `}
+      </Script>
       <QueryClientProvider client={queryClient}>
         <ChakraProvider resetCSS theme={theme}>
           <Layout>
